2315f2cb653030a51db964fb5ede29fa19e074ed
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-07 21:57:05 -0300

    add orders part I

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,28 @@
 import './styles.css'
 import Footer from "../Footer";
 import StepsHeader from "./StepsHeader";
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders(){
+
+    const[products, setProducts] = useState<Product[]>([]);
+
+    console.log(products)
+
+    useEffect(()=>{
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    },[]);
+
     return (
         <>
         <div className="orders-container">
             <StepsHeader/>
+            <ProductsList products={products} />
         </div>
         <Footer />
         </>
---
+++ front-web/src/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_URL= 'http://localhost:8080';
+
+export function fetchProducts(){
+
+    return axios(`${API_URL}/products`);
+    
+}
