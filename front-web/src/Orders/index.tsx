d13ad6854916d3f910116cc34e0441052fe1186a
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-08 21:37:37 -0300

    location save

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -3,13 +3,14 @@ import Footer from "../Footer";
 import StepsHeader from "./StepsHeader";
 import ProductsList from './ProductsList';
 import { useEffect, useState } from 'react';
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
 
 function Orders(){
 
-    const[products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
 
     console.log(products)
 
@@ -24,7 +25,7 @@ function Orders(){
         <div className="orders-container">
             <StepsHeader/>
             <ProductsList products={products} />
-            <OrderLocation />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
         <Footer />
         </>
