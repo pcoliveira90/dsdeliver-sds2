6ae585179badad886b767146a6a2efd82cae8b39
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-08 23:40:18 -0300

    indent

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -30,29 +30,28 @@ function OrderLocation ({onChangeLocation} : Props) {
 
     const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
         const response = await fetchLocalMapBox(inputValue);
-      
         const places = response.data.features.map((item: any) => {
-          return ({
-            label: item.place_name,
-            value: item.place_name,
-            position: {
-              lat: item.center[1],
-              lng: item.center[0]
+            return ({
+                label: item.place_name,
+                value: item.place_name,
+                position: {
+                lat: item.center[1],
+                lng: item.center[0]
             },
             place: item.place_name,
-          });
+        });
         });
         callback(places);
-      };
-      
-      const handleChangeSelect = (place: Place) => {
+    };
+    
+    const handleChangeSelect = (place: Place) => {
         setAddress(place);
         onChangeLocation({
-          latitude: place.position.lat,
-          longitude: place.position.lng,
-          address: place.label!
+            latitude: place.position.lat,
+            longitude: place.position.lng,
+            address: place.label!
         });
-      };
+    };
 
     return (
         <div className="order-location-container">
@@ -66,7 +65,7 @@ function OrderLocation ({onChangeLocation} : Props) {
                         className="filter"
                         loadOptions={loadOptions}
                         onChange={value=>handleChangeSelect(value as Place)}
-                     />
+                    />
                 </div>
                 <MapContainer 
                     center={address.position} 
