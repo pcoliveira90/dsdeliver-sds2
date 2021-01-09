fb569acce6c91eaf7ce078db11bbc88b867c2422
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-08 22:56:47 -0300

    totalizadores em andamento

--- front-web/src/Orders/OrderSummary.tsx
+++ front-web/src/Orders/OrderSummary.tsx
@@ -1,8 +1,27 @@
-function OrderSummary(){
+import { formatPrice } from "./helpers";
+
+type Props={
+    amount:number;
+    totalPrice:number;
+}
+
+function OrderSummary({amount, totalPrice}: Props){
     return(
         <div className="order-summary-container">
             <div className="order-summary-content">
-
+                <div>
+                    <span className="amount-select-container">
+                        <strong className = "amount-selected">{amount}</strong> 
+                        PRODUTOS SELECIONADOS
+                    </span>
+                    <span className="order-summary-total">
+                        <strong className = "amount-selected">{formatPrice(totalPrice)}</strong>
+                        VALOR TOTAL
+                    </span>
+                </div>
+                <button className="order-summary-make-order">
+                    FAZER PEDIDO
+                </button>
             </div>
         </div>
     )
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -7,13 +7,18 @@ import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
 import OrderSummary from './OrderSummary';
+import { checkIsSelected } from './helpers';
 
 function Orders(){
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    
+    const totalPrice = selectedProducts.reduce((sum,item) => {
+        return sum + item.price;
+    },0);
 
-    console.log(products)
 
     useEffect(()=>{
         fetchProducts()
@@ -21,15 +26,35 @@ function Orders(){
             .catch(error => console.log(error))
     },[]);
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts,product);
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return (
         <>
-        <div className="orders-container">
-            <StepsHeader/>
-            <ProductsList products={products} />
-            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
-            <OrderSummary />
-        </div>
-        <Footer />
+            <div className="orders-container">
+                <StepsHeader/>
+                <ProductsList 
+                    products={products} 
+                    onSelectProduct ={handleSelectProduct}
+                    selectedProducts ={selectedProducts}
+                />
+                <OrderLocation 
+                    onChangeLocation={location => setOrderLocation(location)} 
+                />
+                <OrderSummary 
+                    amount={selectedProducts.length}
+                    totalPrice={totalPrice}
+                />
+            </div>
+            <Footer />
         </>
     );
 
