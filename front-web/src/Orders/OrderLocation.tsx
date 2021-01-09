8b43471adbc938b2e2737cb9d7b85d235bd125cf
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-08 21:31:29 -0300

    mapbox

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,11 +1,54 @@
+import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import AsyncSelect from 'react-select/async';
+import { fetchLocalMapBox } from '../api';
 
-const position ={
-    lat: -19.8771011,
-    lng: -43.9375405
+const initialPosition ={
+    lat: -19.8772011,
+    lng: -43.9376405
 } 
 
+type Place={
+    label?:string;
+    value?:string;
+    position :{
+        lat:number;
+        lng:number;
+    }
+}
+
 function OrderLocation() {
+
+    const [address, setAddress]= useState<Place>({
+        position:initialPosition
+    })
+
+    const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
+        const response = await fetchLocalMapBox(inputValue);
+      
+        const places = response.data.features.map((item: any) => {
+          return ({
+            label: item.place_name,
+            value: item.place_name,
+            position: {
+              lat: item.center[1],
+              lng: item.center[0]
+            },
+            place: item.place_name,
+          });
+        });
+        callback(places);
+      };
+      
+      const handleChangeSelect = (place: Place) => {
+        setAddress(place);
+       /* onChangeLocation({
+          latitude: place.position.lat,
+          longitude: place.position.lng,
+          address: place.label!
+        });*/
+      };
+
     return (
         <div className="order-location-container">
             <div className="order-location-content">
@@ -13,15 +56,26 @@ function OrderLocation() {
                     Selecione onde o pedido deve ser entregue:
                 </h3>
                 <div className="filter-container">
+                    <AsyncSelect
+                        placeholder="Digite um endereço para entregar o pedido"
+                        className="filter"
+                        loadOptions={loadOptions}
+                        onChange={value=>handleChangeSelect(value as Place)}
+                     />
                 </div>
-                <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <MapContainer 
+                    center={address.position} 
+                    zoom={13} 
+                    key={address.position.lat}
+                    scrollWheelZoom={true}
+                >
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
+                    <Marker position={address.position}>
                         <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
+                            {address.label}
                         </Popup>
                     </Marker>
                 </MapContainer>
