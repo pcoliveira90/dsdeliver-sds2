7aea1aee346c182dcb498c5b5bdf319a633ad9d2
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-09 11:34:15 -0300

    ajustes chamadas app

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,10 @@
-import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
-import { AppLoading } from 'expo';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import  AppLoading  from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 
@@ -19,9 +19,8 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-      <Header />
       <StatusBar style="auto" />
-      <Home  />
+      <Routes/>
     </View>
   );
 }
