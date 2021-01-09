8d6c11e2c6d224e7a8e9aba732b3e3a3f186ac9e
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-09 11:33:23 -0300

    inital orders

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import Reader from '../Header';
+
+function Orders(){
+    return (
+    <>
+      <Reader/>
+      <View>
+          <Text>XXX</Text>
+      </View>
+    </>     
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Orders;
