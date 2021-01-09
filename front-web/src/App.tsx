55a3f1f9bf889cc19df0f42456b4652fef3ca86c
PAULA CRISTINA DE OLIVEIRA <paulacristina.o@gmail.com>
2021-01-08 23:34:36 -0300

    remove import

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,4 @@
-import { Route } from 'react-router-dom';
 import './App.css';
-import Footer from './Footer';
-import Home from './Home';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import Routes from './Routes';
